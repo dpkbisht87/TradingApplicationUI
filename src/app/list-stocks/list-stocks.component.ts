1f53c1e2ae7f78d7917f2ffa8a713ddab0ec5ce8
deepak.bisht <deepak.bisht@oracle.com>
2021-08-29 20:47:04 +0530

    Initial commit

---
+++ src/app/list-stocks/list-stocks.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { RestApiService } from "../shared/rest-api.service";
+
+@Component({
+  selector: 'app-list-stocks',
+  templateUrl: './list-stocks.component.html',
+  styleUrls: ['./list-stocks.component.css']
+})
+export class ListStocksComponent implements OnInit {
+
+  Stock: any = [];
+
+  constructor(
+    public restApi: RestApiService
+  ) { }
+
+  ngOnInit() {
+    this.loadStocks()
+  }
+
+  // Get employees list
+  loadStocks() {
+    return this.restApi.getStocks().subscribe((data: {}) => {
+      this.Stock = data;
+      let i = 1
+      console.log('id: ' + this.Stock[i].id);
+      console.log('name: ' + this.Stock[i].name);
+      console.log('currentPrice: ' + this.Stock[i].currentPrice);
+      console.log('lastUpdate: ' + this.Stock[i].lastUpdate);
+      console.log('locked: ' + this.Stock[i].locked);
+    })
+  }
+
+  // Delete employee
+  deleteStock(id:string) {
+    if (window.confirm('Are you sure, you want to delete?')) {
+      this.restApi.deleteStock(id).subscribe(data => {
+        this.loadStocks()
+      })
+    }
+  }
+}
